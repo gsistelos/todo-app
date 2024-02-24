92edc2d50e2d62915b1a68c47022128733586a11
gsistelos <gabrielsistelos@gmail.com>
2024-02-24 02:30:47 -0300

    refactor: Home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,11 +6,11 @@ import Header from "./components/Header";
 const Home = () => {
   const { lights, updateTheme } = ThemeValues();
 
-  const themeClasses = lights ? "bg-white text-black" : "bg-black text-white";
+  const mainTheme = lights ? "text-black bg-white" : "text-white bg-black";
 
   return (
     <ThemeContext.Provider value={{ lights, updateTheme }}>
-      <main className={`min-h-screen ${themeClasses}`}>
+      <main className={`min-h-screen ${mainTheme}`}>
         <Header title="Home" />
         <div className="p-4">
           <h1>Home</h1>
